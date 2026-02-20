e11bb18c0b1630c72db5379ff114e43a0bca4e16
SushantPatil04 <patilsushant9767@gmail.com>
2026-02-20 19:38:28 +0530

    Task 2: Create a Personalized Greeting
    output : 
    enter a first name :sushant
    enter a second name :patil
    
    Hello ,sushant patil! Welcome to the python program.

---
+++ Task2.py
@@ -0,0 +1,11 @@
+#personalised greeting
+
+a = input("enter a first name :")
+b = input("enter a second name :")
+
+print(f"\nHello ,{a} {b}! Welcome to the python program. ")
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print(f"\nHello, {first_name} {last_name}! Welcome to the Python program.")
